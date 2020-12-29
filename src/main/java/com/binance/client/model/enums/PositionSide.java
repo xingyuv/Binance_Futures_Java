ff70e66d9bebc9c99108bec1bf96b9391291abb3
xingyu4j <xingyu4j@vip.qq.com>
2020-12-29 17:33:20 +0800

    【修改】增加注释

--- src/main/java/com/binance/client/model/enums/PositionSide.java
+++ src/main/java/com/binance/client/model/enums/PositionSide.java
@@ -4,11 +4,17 @@ package com.binance.client.model.enums;
  * @author xingyu
  */
 public enum PositionSide {
-
+    /**
+     * 双向
+     */
     BOTH("BOTH"),
-
+    /**
+     * 空
+     */
     SHORT("SHORT"),
-
+    /**
+     * 多
+     */
     LONG("LONG"),
     ;
 
